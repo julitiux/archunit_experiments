15b2fa22f1c0aa3c103b94837a8f043389ad3e3c
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-12-18 23:37:42 -0600

    starting new project archunit_experiments

---
+++ src/main/java/com/archunit_experiments/ArchunitExperimentsApplication.java
@@ -0,0 +1,13 @@
+package com.archunit_experiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchunitExperimentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchunitExperimentsApplication.class, args);
+	}
+
+}
