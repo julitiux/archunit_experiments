3839e3423de696cccc6946e56251afd38815554a
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-12-27 00:02:49 -0600

    rename Test.java -> User.java

--- src/main/java/com/archunit_experiments/domain/Test.java
+++
@@ -1,7 +0,0 @@
-package com.archunit_experiments.domain;
-
-public class Test {
-  private String test1;
-  private String test2;
-  private String test3;
-}
---
+++ src/main/java/com/archunit_experiments/domain/User.java
@@ -0,0 +1,7 @@
+package com.archunit_experiments.domain;
+
+public class User {
+  private String firstName;
+  private String lastName;
+  private String email;
+}
