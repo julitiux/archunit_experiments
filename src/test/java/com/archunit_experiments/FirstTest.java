657095529e1776a3da75c9cc09a0bda663a26251
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-12-31 23:54:56 -0600

    using the tag @Slf4j for log.info into test getClazz

--- src/test/java/com/archunit_experiments/FirstTest.java
+++ src/test/java/com/archunit_experiments/FirstTest.java
@@ -4,9 +4,11 @@ import com.archunit_experiments.domain.User;
 import com.tngtech.archunit.core.domain.JavaClass;
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+@Slf4j
 public class FirstTest {
 
   @Test
@@ -31,15 +33,15 @@ public class FirstTest {
     JavaClasses javaClasses = new ClassFileImporter().importPackages("com.archunit_experiments");
     JavaClass clazz = javaClasses.get(User.class);
 
-    System.out.println(clazz.getSimpleName());
-    System.out.println(clazz.getAccessesFromSelf());
-    System.out.println(clazz.getAccessesToSelf());
-    System.out.println(clazz.getAllAccessesFromSelf());
-    System.out.println(clazz.getAllFields());
-    System.out.println(clazz.getAllClassesSelfIsAssignableTo());
-    System.out.println(clazz.getAllConstructors());
-    System.out.println(clazz.getAllMembers());
-    System.out.println(clazz.getAllRawInterfaces());
+    log.info("{}", clazz.getSimpleName());
+    log.info("{}", clazz.getAccessesFromSelf());
+    log.info("{}", clazz.getAccessesToSelf());
+    log.info("{}", clazz.getAllAccessesFromSelf());
+    log.info("{}", clazz.getAllFields());
+    log.info("{}", clazz.getAllClassesSelfIsAssignableTo());
+    log.info("{}", clazz.getAllConstructors());
+    log.info("{}", clazz.getAllMembers());
+    log.info("{}", clazz.getAllRawInterfaces());
   }
 
 }
