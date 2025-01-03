67ef9b916f4ef362cd290c98310214718b75c616
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-01-03 02:08:44 -0600

    implement test unit testPackageDependencyChecks

--- src/test/java/com/archunit_experiments/PackageDependencyChecksTest.java
+++ src/test/java/com/archunit_experiments/PackageDependencyChecksTest.java
@@ -1,4 +1,22 @@
 package com.archunit_experiments;
 
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.ArchRule;
+import org.junit.jupiter.api.Test;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
 public class PackageDependencyChecksTest {
+
+  @Test
+  void testPackageDependencyChecks() {
+
+    JavaClasses javaClasses = new ClassFileImporter().importPackages("");
+
+    ArchRule rule = noClasses().that().resideInAPackage("..source..")
+      .should().dependOnClassesThat().resideInAPackage("..foo..");
+
+    rule.check(javaClasses);
+  }
 }
