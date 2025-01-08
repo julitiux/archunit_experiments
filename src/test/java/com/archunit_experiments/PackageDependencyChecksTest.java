5d57c6201b81db0fe2729b85c3a8b1b9bba7d915
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-01-08 01:23:45 -0600

    use the ArchRuleDefinition

--- src/test/java/com/archunit_experiments/PackageDependencyChecksTest.java
+++ src/test/java/com/archunit_experiments/PackageDependencyChecksTest.java
@@ -3,9 +3,9 @@ package com.archunit_experiments;
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
 import org.junit.jupiter.api.Test;
 
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
 public class PackageDependencyChecksTest {
 
@@ -14,7 +14,7 @@ public class PackageDependencyChecksTest {
 
     JavaClasses javaClasses = new ClassFileImporter().importPackages("com.archunit_experiments.package_dependency_checks");
 
-    ArchRule rule = noClasses().that().resideInAPackage("..source..")
+    ArchRule rule = ArchRuleDefinition.noClasses().that().resideInAPackage("..source..")
       .should().dependOnClassesThat().resideInAPackage("..foo..");
 
     rule.check(javaClasses);
